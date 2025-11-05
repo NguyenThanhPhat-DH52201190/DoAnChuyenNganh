a562d697e43c3c3f398879f44a355ebf2bfce8c9
NguyenThanhPhat-DH52201190 <thanhphatntp1202@gmail.com>
2025-11-05 10:40:37 +0700

    Cập nhật code mới

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
