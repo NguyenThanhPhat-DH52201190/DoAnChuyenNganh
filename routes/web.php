0b5d617a8f3670ceae9dfc6223e44397d71c5511
NguyenThanhPhat-DH52201190 <thanhphatntp1202@gmail.com>
2025-11-08 22:06:01 +0700

    Cập nhật code mới

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,50 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin');
+Route::get('/admin/category', function () {
+    return view('admin/category/category_list');
+})->name('category');
+Route::get('/admin/product', function () {
+    return view('admin/product/product_list');
+})->name('product');
+Route::get('/admin/register', function () {
+    return view('admin/register/register');
+})->name('register');
+Route::get('/admin/forgotpassword', function () {
+    return view('admin/forgot/forgotpassword');
+})->name('forgotpassword');
+Route::get('/admin/Button', function () {
+    return view('admin/Components/Button');
+})->name('Button');
+Route::get('/admin/Card', function () {
+    return view('admin/Components/Card');
+})->name('Card');
 
+Route::get('/admin/other', function () {
+    return view('admin/ul/other');
+})->name('other');
+Route::get('/admin/colors', function () {
+    return view('admin/ul/colors');
+})->name('border');
+Route::get('/admin/border', function () {
+    return view('admin/ul/border');
+})->name('border');
+Route::get('/admin/animation', function () {
+    return view('admin/ul/animation');
+})->name('animation');
+Route::get('/admin/chart', function () {
+    return view('admin/chart/chart');
+})->name('chart');
+Route::get('/admin/table', function () {
+    return view('admin/table/table');
+})->name('table');
+Route::get('/admin/blank', function () {
+    return view('admin/Page/blank');
+})->name('blank');
+Route::get('/admin/404', function () {
+    return view('admin/Page/404');
+})->name('404');
